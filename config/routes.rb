87b296efbd725155b299b2e9b773ff2bc0aeb351
Eric Suescun <edsuescun@gmail.com>
2019-04-14 18:03:33 -0500

    Se crea la ruta Edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	get 'questions', to: 'questions#index'
 	get '/questions/new', to: 'questions#new', as: 'new_question'
 	post '/questions', to: 'questions#create'
-	get '/questions/:id', to: 'questions#show'
+	get '/questions/:id', to: 'questions#show', as: 'question'
+	get '/questions/:id/edit', to: 'questions#edit'
 end
