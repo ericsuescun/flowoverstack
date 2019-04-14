1589166bc741be3e76deb4ebe313b40660dd80b0
Eric Suescun <edsuescun@gmail.com>
2019-04-14 17:52:51 -0500

    Se implementa la creación de nuevas preguntas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	get 'questions', to: 'questions#index'
-	get '/questions/new', to: 'questions#new'
+	get '/questions/new', to: 'questions#new', as: 'new_question'
+	post '/questions', to: 'questions#create'
 end
