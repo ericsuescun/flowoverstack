fd9c50d572432348f74c3903cbbc4bfd07a17c10
Eric Suescun <edsuescun@gmail.com>
2019-04-14 17:57:24 -0500

    Se implementa la accion Show para Question

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	get 'questions', to: 'questions#index'
 	get '/questions/new', to: 'questions#new', as: 'new_question'
 	post '/questions', to: 'questions#create'
+	get '/questions/:id', to: 'questions#show'
 end
