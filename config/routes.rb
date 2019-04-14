879e0537aac8229e27401adf3016cd6ffc93c722
Eric Suescun <edsuescun@gmail.com>
2019-04-14 18:11:42 -0500

    Se implementan los link para Editar las preguntas

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 	get '/questions/new', to: 'questions#new', as: 'new_question'
 	post '/questions', to: 'questions#create'
 	get '/questions/:id', to: 'questions#show', as: 'question'
-	get '/questions/:id/edit', to: 'questions#edit'
+	get '/questions/:id/edit', to: 'questions#edit', as: 'edit_question'
 	patch '/questions/:id', to: 'questions#update'
 end
