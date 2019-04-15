545ee50acc29c866cee804bd0b921ff61efe8bf8
Eric Suescun <edsuescun@gmail.com>
2019-04-14 21:58:37 -0500

    Se implementa la capacidad de registrar usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+	resources :users, only: [:new, :create]
+
 	root 'questions#index'
 	# get '/questions', to: 'questions#index'
 	# get '/questions/new', to: 'questions#new', as: 'new_question'
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_222756) do
+ActiveRecord::Schema.define(version: 2019_04_15_025222) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", limit: 100
+    t.string "password_digest"
+    t.string "name", limit: 100
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
