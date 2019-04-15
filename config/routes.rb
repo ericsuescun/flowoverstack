163deee64046336070b838686387fd9bf5d1555f
Eric Suescun <edsuescun@gmail.com>
2019-04-14 22:15:32 -0500

    Se implementa la capacidad de registrarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-	resources :users, only: [:new, :create]
-
 	root 'questions#index'
-	# get '/questions', to: 'questions#index'
-	# get '/questions/new', to: 'questions#new', as: 'new_question'
-	# post '/questions', to: 'questions#create'
-	# get '/questions/:id', to: 'questions#show', as: 'question'
-	# get '/questions/:id/edit', to: 'questions#edit', as: 'edit_question'
-	# patch '/questions/:id', to: 'questions#update'
 
 	resources :questions
+
+	resources :users, only: [:new, :create]
+
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
 end
