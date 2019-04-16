bfd0597c117b9ed3b7274d2b60a9df5660e808d2
Eric Suescun <edsuescun@gmail.com>
2019-04-15 22:47:38 -0500

    Ejecuto annotate y este hace su magia

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id          :integer          not null, primary key
+#  question_id :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Vote < ApplicationRecord
   belongs_to :question
   belongs_to :user
