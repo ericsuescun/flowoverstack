397afbd5c4a6d9c05317f124fc124361d2c5b41d
Eric Suescun <edsuescun@gmail.com>
2019-04-22 09:34:59 -0500

    Se monta de nuevo segun GoRails video para revisar que es lo que pasa con el polymorphism

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,15 @@
 class CommentsController < ApplicationController
 	#Require authentication first!!!
-	before_action :find_commentable, only: :create
 
 	def create
-	  @commentable.comments.build(comment_params)
-	  @commentable.save
-
+	  @comment = @commentable.comments.new(comment_params)
 	  @comment.user = current_user
+	  @commentable.save
 	  redirect_to @commentable, notice: "Comentario publicado!"
 	end
 
 	private
 	  def comment_params
-	    params.require(:comment).permit(:body, :answer_id).merge(user: current_user)
+	    params.require(:comment).permit(:body).merge(user: current_user)
 	  end
-
-	  def find_commentable
-	    if params[:answer_id]
-	      @commentable = Comment.find_by_id(params[:answer_id]) 
-	    elsif params[:question_id]
-	      @commentable = Post.find_by_id(params[:question_id])
-	    end
-	  end
-
 end
